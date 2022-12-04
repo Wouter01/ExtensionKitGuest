72a3e8f3f181c1f4fcaa59b2d3cde84890d40274
Wouter01 <wouterhennen@gmail.com>
2022-12-04 04:18:13 +0100

    Initial Commit

---
+++ ExtensionKitGuest/ExtensionKitGuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionKitGuestApp.swift
+//  ExtensionKitGuest
+//
+//  Created by Wouter Hennen on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExtensionKitGuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
