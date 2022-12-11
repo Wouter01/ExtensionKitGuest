1af451d763981a89fb4702378a7467b245458477
Wouter01 <wouterhennen@gmail.com>
2022-12-11 20:04:01 +0100

    Added CEEnvironment for passing environment values to an extension

--- AmazingTheme/AmazingTheme.swift
+++ AmazingTheme/AmazingTheme.swift
@@ -29,8 +29,21 @@ struct AmazingTheme: SettingsExtension {
 
 struct SettingsView: View {
     @State var test = false
-    
+
+    @CEEnvironment(\.testkey) var testkey
+    @CEEnvironment(\.testkey2) var testkey2
+    @CEEnvironment(\.complexValue) var complexValue
+
     var body: some View {
+        ForEach(complexValue, id: \.self) {
+            Text($0)
+        }
+        .foregroundColor(.accentColor)
+
         Toggle("Test toggle", isOn: $test)
+
+        Toggle("Testkey", isOn: .constant(testkey))
+        Toggle("Testkey2", isOn: .constant(testkey2))
+
     }
 }
